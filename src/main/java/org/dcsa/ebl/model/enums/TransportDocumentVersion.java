5fe6b8ec8d288278119b531a7392646eae65507f
Henrik Helmø Larsen <hhl@asseco.dk>
2020-12-08 14:33:04 +0100

    Update enums

---
+++ src/main/java/org/dcsa/ebl/model/enums/TransportDocumentVersion.java
@@ -0,0 +1,14 @@
+package org.dcsa.ebl.model.enums;
+
+public enum TransportDocumentVersion {
+    RECE,
+    DRFT,
+    PENA,
+    PENU,
+    REJE,
+    APPR,
+    ISSU,
+    SURR,
+    SUBM,
+    VOID;
+}
