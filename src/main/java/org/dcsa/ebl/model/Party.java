5e538877c2a0a4747adfffd2945dc077bfdc1f00
Henrik Helmø Larsen <hhl@asseco.dk>
2020-12-08 15:06:42 +0100

    Party updates

--- src/main/java/org/dcsa/ebl/model/Party.java
+++ src/main/java/org/dcsa/ebl/model/Party.java
@@ -3,6 +3,7 @@ package org.dcsa.ebl.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.dcsa.core.model.GetId;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.Size;
@@ -14,12 +15,49 @@ import java.util.UUID;
 public class Party implements GetId<UUID> {
     private UUID id;
 
+    @Column("party_name")
     @Size(max = 100)
-    private String party_name;
+    private String partyName;
 
+    @Column("tax_reference_1")
     @Size(max = 20)
-    private String tax_reference;
+    private String taxReference1;
 
+    @Column("tax_reference_2")
+    @Size(max = 20)
+    private String taxReference2;
+
+    @Column("public_key")
     @Size(max = 500)
-    private String public_key;
+    private String publicKey;
+
+    @Column("street_name")
+    @Size(max = 100)
+    private String streetName;
+
+    @Column("street_number")
+    @Size(max = 50)
+    private String streetNumber;
+
+    @Size(max = 50)
+    private String floor;
+
+    @Column("postal_code")
+    @Size(max = 10)
+    private String postalCode;
+
+    @Column("city_name")
+    @Size(max = 65)
+    private String cityName;
+
+    @Column("state_region")
+    @Size(max = 65)
+    private String stateRegion;
+
+    @Size(max = 75)
+    private String country;
+
+    @Column("nmfta_code")
+    @Size(max = 4)
+    private String nmftaCode;
 }
