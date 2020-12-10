6e347954ad858d2eeeedaf349f09dc43e277c7a2
Niels Thykier <nt@asseco.dk>
2020-12-10 16:16:58 +0100

    Map ShipmentLocation on POST /shipping-instructions
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ src/main/java/org/dcsa/ebl/service/impl/ShipmentLocationServiceImpl.java
@@ -0,0 +1,35 @@
+package org.dcsa.ebl.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
+import org.dcsa.ebl.model.ShipmentLocation;
+import org.dcsa.ebl.repository.ShipmentLocationRepository;
+import org.dcsa.ebl.service.ShipmentLocationService;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+@Service
+public class ShipmentLocationServiceImpl extends ExtendedBaseServiceImpl<ShipmentLocationRepository, ShipmentLocation, UUID> implements ShipmentLocationService {
+    private final ShipmentLocationRepository shipmentLocationRepository;
+
+    @Override
+    public ShipmentLocationRepository getRepository() {
+        return shipmentLocationRepository;
+    }
+
+    @Override
+    public Class<ShipmentLocation> getModelClass() {
+        return ShipmentLocation.class;
+    }
+
+    public Flux<ShipmentLocation> createAll(Flux<ShipmentLocation> shipmentLocations) {
+        return shipmentLocations
+                .concatMap(this::preCreateHook)
+                .buffer(70)
+                .concatMap(shipmentLocationRepository::saveAll);
+    }
+}
