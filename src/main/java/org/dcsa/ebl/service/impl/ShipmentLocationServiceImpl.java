6676a6d53364c0b69d5111d268345b1225cf1e7b
Niels Thykier <nt@asseco.dk>
2020-12-17 15:43:36 +0100

    Assume ShipmentLocation exist
    Closes: #26
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/service/impl/ShipmentLocationServiceImpl.java
+++ src/main/java/org/dcsa/ebl/service/impl/ShipmentLocationServiceImpl.java
@@ -3,10 +3,12 @@ package org.dcsa.ebl.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
 import org.dcsa.ebl.model.ShipmentLocation;
+import org.dcsa.ebl.model.enums.ShipmentLocationType;
 import org.dcsa.ebl.repository.ShipmentLocationRepository;
 import org.dcsa.ebl.service.ShipmentLocationService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.UUID;
@@ -38,5 +40,10 @@ public class ShipmentLocationServiceImpl extends ExtendedBaseServiceImpl<Shipmen
     @Override
     public Flux<ShipmentLocation> findAllByShipmentIDIn(List<UUID> shipmentIDs) {
         return shipmentLocationRepository.findAllByShipmentIDIn(shipmentIDs);
+    }
+
+    @Override
+    public Mono<ShipmentLocation> findByShipmentIDAndLocationTypeAndLocationID(UUID shipmentID, ShipmentLocationType shipmentLocationType, UUID locationID) {
+        return shipmentLocationRepository.findByShipmentIDAndLocationTypeAndLocationID(shipmentID, shipmentLocationType, locationID);
     }
 }
