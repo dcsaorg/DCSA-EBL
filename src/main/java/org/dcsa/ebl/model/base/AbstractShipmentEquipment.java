b86514a6a09f5100de7b16208098dd8836df96f4
Niels Thykier <nt@asseco.dk>
2020-12-17 12:58:44 +0100

    Extract AbstractShipmentEquipment
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ src/main/java/org/dcsa/ebl/model/base/AbstractShipmentEquipment.java
@@ -0,0 +1,33 @@
+package org.dcsa.ebl.model.base;
+
+import lombok.Data;
+import org.dcsa.ebl.model.enums.WeightUnit;
+import org.springframework.data.relational.core.mapping.Column;
+
+import javax.validation.constraints.Size;
+
+@Data
+public abstract class AbstractShipmentEquipment {
+
+    @Column("equipment_reference")
+    @Size(max = 15)
+    private String equipmentReference;
+
+    @Column("verified_gross_mass")
+    private String verifiedGrossMass;
+
+    @Column("cargo_gross_weight")
+    private Float cargoGrossWeight;
+
+    @Column("cargo_gross_weight_unit")
+    @Size(max = 3)
+    private WeightUnit cargoGrossWeightUnit;
+
+    public void setCargoGrossWeightUnit(String cargoGrossWeightUnit) {
+        this.cargoGrossWeightUnit = WeightUnit.valueOf(cargoGrossWeightUnit);
+    }
+
+    public void setCargoGrossWeightUnit(WeightUnit cargoGrossWeightUnit) {
+        this.cargoGrossWeightUnit = cargoGrossWeightUnit;
+    }
+}
