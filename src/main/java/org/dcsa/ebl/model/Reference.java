eb6f6259540bc2ef60ca1abd13b0d413aacb0719
Niels Thykier <nt@asseco.dk>
2020-12-18 12:38:26 +0100

    Correct Table and Id of Reference model
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/model/Reference.java
+++ src/main/java/org/dcsa/ebl/model/Reference.java
@@ -8,16 +8,20 @@ import org.dcsa.core.model.AuditBase;
 import org.dcsa.core.model.GetId;
 import org.dcsa.ebl.model.enums.ReferenceTypeCode;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.Size;
 import java.util.UUID;
 
+// REFERENCES is an SQL keyword (as well as the name of the table)
+// Use magic quotes to work around the name clash.
+@Table("\"references\"")
 @NoArgsConstructor
 @Data
 public class Reference extends AuditBase implements GetId<UUID> {
 
     @JsonProperty("referenceID")
-    @Column("reference_id")
+    @Column("id")
     private UUID referenceID;
 
     private ReferenceTypeCode referenceType;
