847b5a81efa65c83c156cb0adac06e6e8c65f0b2
Niels Thykier <nt@asseco.dk>
2020-12-28 14:43:57 +0100

    Initial untested implementation of PATCH/PUT /shipping-instructions/{ID}
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/repository/CargoItemRepository.java
+++ src/main/java/org/dcsa/ebl/repository/CargoItemRepository.java
@@ -3,11 +3,14 @@ package org.dcsa.ebl.repository;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.ebl.model.CargoItem;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CargoItemRepository extends ExtendedRepository<CargoItem, UUID> {
 
     Flux<CargoItem> findAllByShippingInstructionID(UUID shippingInstructionID);
+    Mono<Void> deleteAllByIdIn(List<UUID> cargoItemIDs);
 
 }
--- src/main/java/org/dcsa/ebl/service/CargoItemService.java
+++ src/main/java/org/dcsa/ebl/service/CargoItemService.java
@@ -3,10 +3,13 @@ package org.dcsa.ebl.service;
 import org.dcsa.core.service.ExtendedBaseService;
 import org.dcsa.ebl.model.CargoItem;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CargoItemService extends ExtendedBaseService<CargoItem, UUID> {
 
     Flux<CargoItem> findAllByShippingInstructionID(UUID shippingInstructionID);
+    Mono<Void> deleteAllByIdIn(List<UUID> cargoItemIDs);
 }
