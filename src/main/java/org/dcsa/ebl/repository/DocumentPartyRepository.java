80f8ac433845257c3afa1f8bfc585a2acd4e42c4
Niels Thykier <nt@asseco.dk>
2020-12-29 16:38:32 +0100

    Fix update of DocumentParty
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/repository/DocumentPartyRepository.java
+++ src/main/java/org/dcsa/ebl/repository/DocumentPartyRepository.java
@@ -2,10 +2,13 @@ package org.dcsa.ebl.repository;
 
 import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.ebl.model.DocumentParty;
+import org.dcsa.ebl.model.enums.PartyFunction;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface DocumentPartyRepository extends ExtendedRepository<DocumentParty, UUID> {
     Flux<DocumentParty> findAllByShippingInstructionID(UUID shippingInstructionID);
+    Mono<DocumentParty> findByPartyIDAndPartyFunction(UUID partyID, PartyFunction partyFunction);
 }
