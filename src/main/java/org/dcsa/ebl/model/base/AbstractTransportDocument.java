20a7405282220d6b90eab33889c050e64704e9f9
Henrik Helmø Larsen <hhl@asseco.dk>
2021-01-07 21:52:13 +0100

    Inital commit

---
+++ src/main/java/org/dcsa/ebl/model/base/AbstractTransportDocument.java
@@ -0,0 +1,59 @@
+package org.dcsa.ebl.model.base;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.dcsa.core.model.AuditBase;
+import org.dcsa.core.model.GetId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public abstract class AbstractTransportDocument extends AuditBase implements GetId<UUID> {
+
+    @Id
+    @JsonProperty("transportDocumentID")
+    private UUID id;
+
+    @Column("place_of_issue")
+    private UUID placeOfIssue;
+
+    @Column("date_of_issue")
+    private LocalDate dateOfIssue;
+
+    @Column("onboard_date")
+    private LocalDate onboardDate;
+
+    @Column("received_for_shipment_date")
+    private LocalDate receivedForShipmentDate;
+
+    @Column("document_reference_number")
+    @Size(max = 20)
+    private String documentReferenceNumber;
+
+    @Column("terms_and_conditions")
+    private String termsAndConditions;
+
+    @Column("issuer")
+    private UUID issuer;
+
+    @Column("shipping_instruction_id")
+    private UUID shippingInstructionID;
+
+    @Column("declared_value_currency")
+    @Size(max = 3)
+    private String declaredValueCurrency;
+
+    @Column("declared_value")
+    private Float declaredValue;
+
+    @Column("number_of_rider_pages")
+    private Integer numberOfRiderPages;
+}
--- src/main/java/org/dcsa/ebl/service/TransportDocumentTOService.java
+++ src/main/java/org/dcsa/ebl/service/TransportDocumentTOService.java
@@ -1,9 +1,20 @@
 package org.dcsa.ebl.service;
 
+import org.dcsa.core.extendedrequest.ExtendedRequest;
 import org.dcsa.core.service.ExtendedBaseService;
+import org.dcsa.ebl.model.ShippingInstruction;
+import org.dcsa.ebl.model.TransportDocument;
+import org.dcsa.ebl.model.transferobjects.ShippingInstructionTO;
 import org.dcsa.ebl.model.transferobjects.TransportDocumentTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-public interface TransportDocumentTOService extends ExtendedBaseService<TransportDocumentTO, UUID> {
+public interface TransportDocumentTOService {
+
+    Mono<TransportDocumentTO> findById(UUID uuid);
+    // Use of ShippingInstruction is deliberate because we do not support filtering on any other fields then
+    // those provided in ShippingInstruction.class
+    Flux<TransportDocumentTO> findAllExtended(final ExtendedRequest<TransportDocument> extendedRequest);
 }
