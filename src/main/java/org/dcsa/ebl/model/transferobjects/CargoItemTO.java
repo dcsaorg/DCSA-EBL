3abe33be885e3d890ef289de0ea967adc9a480ae
Niels Thykier <nt@asseco.dk>
2021-01-19 21:38:47 +0100

    Move carrierBookingReference to CargoItemTOs
    Closes: #41
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/model/transferobjects/CargoItemTO.java
+++ src/main/java/org/dcsa/ebl/model/transferobjects/CargoItemTO.java
@@ -6,11 +6,11 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.dcsa.ebl.model.CargoLineItem;
 import org.dcsa.ebl.model.base.AbstractCargoItem;
+import org.springframework.data.annotation.Transient;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
-import java.util.UUID;
 
 @NoArgsConstructor
 @Data
@@ -21,5 +21,9 @@ public class CargoItemTO extends AbstractCargoItem {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @Size(max = 15)
     private String equipmentReference;
+
+    @NotNull
+    @Transient
+    private String carrierBookingReference;
 
 }
--- src/main/java/org/dcsa/ebl/repository/ShipmentRepository.java
+++ src/main/java/org/dcsa/ebl/repository/ShipmentRepository.java
@@ -2,11 +2,13 @@ package org.dcsa.ebl.repository;
 
 import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.ebl.model.Shipment;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ShipmentRepository extends ExtendedRepository<Shipment, UUID> {
 
-    Mono<Shipment> findByCarrierBookingReference(String carrierBookingReference);
+    Flux<Shipment> findByCarrierBookingReferenceIn(List<String> carrierBookingReferences);
 }
