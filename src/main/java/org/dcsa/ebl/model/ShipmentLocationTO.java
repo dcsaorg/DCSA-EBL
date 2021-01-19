514b12a5b5f5358971fc3a3b630bc1845e768691
Niels Thykier <nt@asseco.dk>
2021-01-19 12:06:05 +0100

    EBL: ShipmentLocation must not leak ShipmentID
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ src/main/java/org/dcsa/ebl/model/ShipmentLocationTO.java
@@ -0,0 +1,11 @@
+package org.dcsa.ebl.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.dcsa.ebl.model.base.AbstractShipmentLocation;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ShipmentLocationTO extends AbstractShipmentLocation {
+
+}
