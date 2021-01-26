e3aac8c795a58b5539ced813027fbc8dd01c5d58
Henrik Helmø Larsen <hhl@asseco.dk>
2021-01-26 13:34:30 +0100

    Remove Booking from EBL

--- src/main/java/org/dcsa/ebl/repository/BookingTORepository.java
+++
@@ -1,8 +0,0 @@
-package org.dcsa.ebl.repository;
-
-import org.dcsa.core.repository.ExtendedRepository;
-import org.dcsa.ebl.model.transferobjects.BookingTO;
-
-public interface BookingTORepository extends ExtendedRepository<BookingTO, String> {
-
-}
