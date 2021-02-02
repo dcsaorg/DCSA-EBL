cfdbfd2faf977ea9260e6ea4c7179d1bf4a35e4f
Niels Thykier <nt@asseco.dk>
2021-02-02 12:55:40 +0100

    Move some code from SITOSI to DocumentPartyService
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/service/DocumentPartyService.java
+++ src/main/java/org/dcsa/ebl/service/DocumentPartyService.java
@@ -3,6 +3,7 @@ package org.dcsa.ebl.service;
 import org.dcsa.core.service.ExtendedBaseService;
 import org.dcsa.ebl.model.DocumentParty;
 import org.dcsa.ebl.model.enums.PartyFunction;
+import org.dcsa.ebl.model.transferobjects.DocumentPartyTO;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -12,4 +13,5 @@ public interface DocumentPartyService extends ExtendedBaseService<DocumentParty,
     Flux<DocumentParty> findAllByShippingInstructionID(UUID shippingInstructionID);
     Mono<DocumentParty> findByPartyIDAndPartyFunction(UUID partyID, PartyFunction partyFunction);
     Mono<Integer> deleteByPartyIDAndPartyFunctionAndShipmentID(UUID partyID, PartyFunction partyFunction, UUID shipmentID);
+    Mono<Void> deleteObsoleteDocumentPartyInstances(Iterable<DocumentPartyTO> documentPartyTOs);
 }
