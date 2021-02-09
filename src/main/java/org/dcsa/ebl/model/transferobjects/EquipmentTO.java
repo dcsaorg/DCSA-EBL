a04410c3a5334c120af41617f17abc197d03ab43
Niels Thykier <nt@asseco.dk>
2021-02-09 10:09:09 +0100

    Rewrite Equipment handling to support SOC
    Closes: #53
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/model/transferobjects/EquipmentTO.java
+++ src/main/java/org/dcsa/ebl/model/transferobjects/EquipmentTO.java
@@ -3,7 +3,13 @@ package org.dcsa.ebl.model.transferobjects;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.dcsa.ebl.model.Equipment;
 import org.dcsa.ebl.model.base.AbstractEquipment;
+import org.dcsa.ebl.model.utils.MappingUtil;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -15,6 +21,33 @@ public class EquipmentTO extends AbstractEquipment {
             EquipmentTO p = new EquipmentTO();
             p.setEquipmentReference(this.getEquipmentReference());
             return this.equals(p);
+        }
+        return false;
+    }
+
+    public Equipment createModifiedCopyOrNull(Equipment equipment) {
+        Equipment clone ;
+        boolean modified = false;
+        if (this.containsOnlyID()) {
+            return null;
+        }
+        clone = MappingUtil.instanceFrom(equipment, Equipment::new, Equipment.class);
+        assert this.getEquipmentReference().equals(equipment.getEquipmentReference());
+
+        modified |= setIfChanged(clone.getWeightUnit(), this.getWeightUnit(), clone::setWeightUnit);
+        modified |= setIfChanged(clone.getTareWeight(), this.getTareWeight(), clone::setTareWeight);
+        modified |= setIfChanged(clone.getIsoEquipmentCode(), this.getIsoEquipmentCode(), clone::setIsoEquipmentCode);
+        if (this.getIsShipperOwned() != null) {
+            modified |= setIfChanged(clone.getIsShipperOwned(), this.getIsShipperOwned(), clone::setIsShipperOwned);
+        }
+
+        return modified ? clone : null;
+    }
+
+    private static <T> boolean setIfChanged(T original, T update, Consumer<T> setter) {
+        if (!Objects.equals(original, update)) {
+            setter.accept(update);
+            return true;
         }
         return false;
     }
--- src/main/java/org/dcsa/ebl/service/impl/EquipmentServiceImpl.java
+++ src/main/java/org/dcsa/ebl/service/impl/EquipmentServiceImpl.java
@@ -1,11 +1,18 @@
 package org.dcsa.ebl.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.dcsa.core.exception.CreateException;
+import org.dcsa.core.exception.UpdateException;
 import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
 import org.dcsa.ebl.model.Equipment;
+import org.dcsa.ebl.model.base.AbstractEquipment;
+import org.dcsa.ebl.model.transferobjects.EquipmentTO;
+import org.dcsa.ebl.model.transferobjects.ShipmentEquipmentTO;
+import org.dcsa.ebl.model.utils.MappingUtil;
 import org.dcsa.ebl.repository.EquipmentRepository;
 import org.dcsa.ebl.service.EquipmentService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
@@ -25,7 +32,46 @@ public class EquipmentServiceImpl extends ExtendedBaseServiceImpl<EquipmentRepos
     }
 
     @Override
-    public Mono<Equipment> createWithId(Equipment equipment) {
-        return equipmentRepository.insert(equipment);
+    public Mono<Void> ensureEquipmentExistAndMatchesRequest(Iterable<ShipmentEquipmentTO> shipmentEquipmentTOs) {
+        return Flux.fromIterable(shipmentEquipmentTOs)
+                .flatMap(shipmentEquipmentTO -> {
+                    EquipmentTO equipmentTO = shipmentEquipmentTO.getEquipment();
+                    String equipmentReference = equipmentTO.getEquipmentReference();
+                    return equipmentRepository.findById(equipmentReference)
+                            .flatMap(resolvedEquipment -> {
+                                Equipment updatedVersion = equipmentTO.createModifiedCopyOrNull(resolvedEquipment);
+                                if (updatedVersion != null) {
+                                    if (!resolvedEquipment.getIsShipperOwned()) {
+                                        return Mono.error(new UpdateException("Cannot modify Carrier owned Equipment (via reference): " + equipmentReference));
+                                    }
+                                    if (!Boolean.TRUE.equals(updatedVersion.getIsShipperOwned())) {
+                                        return Mono.error(new UpdateException("Cannot turn Shipper owner Equipment "
+                                                + equipmentReference + " into a Carrier owned Equipment"
+                                                + " (\"isShipperOwned\" must be true)"
+                                        ));
+                                    }
+                                    return preUpdateHook(resolvedEquipment, updatedVersion)
+                                            .flatMap(this::save);
+                                }
+                                return Mono.just(resolvedEquipment);
+                            }).switchIfEmpty(Mono.defer(() -> {
+                                Equipment equipment;
+                                if (equipmentTO.containsOnlyID()) {
+                                    return Mono.error(new CreateException("Unknown Equipment reference: " + equipmentReference));
+                                }
+                                if (!Boolean.TRUE.equals(equipmentTO.getIsShipperOwned())) {
+                                    return Mono.error(new CreateException("Unknown Equipment reference: " + equipmentReference
+                                            + " (set \"isShipperOwned\" to true to create it if it is your own container)"));
+                                }
+                                equipment = MappingUtil.instanceFrom(equipmentTO, Equipment::new, AbstractEquipment.class);
+                                equipment.setIsShipperOwned(Boolean.TRUE);
+                                return equipmentRepository.insert(equipment);
+                            })).zipWith(Mono.just(shipmentEquipmentTO));
+                }).doOnNext(tuple -> {
+                    Equipment equipment = tuple.getT1();
+                    ShipmentEquipmentTO shipmentEquipmentTO = tuple.getT2();
+                    EquipmentTO newEquipmentTO = MappingUtil.instanceFrom(equipment, EquipmentTO::new, AbstractEquipment.class);
+                    shipmentEquipmentTO.setEquipment(newEquipmentTO);
+                }).then();
     }
 }
