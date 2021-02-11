ca929f3d5a8a97f63ceea5d700d3c33474cabf63
Niels Thykier <nt@asseco.dk>
2021-02-11 07:20:34 +0100

    Rewrite handling of CargoItem and CargoLineItem
    In particular, remove `cargoItemID` from `CargoLineItemTO` as it is
    not present in the swagger spec. This in turn fixes two issues (one
    directly, one from the relevant refactoring to support the change).
    
    Closes: #62
    Closes: #65
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/repository/CargoLineItemRepository.java
+++ src/main/java/org/dcsa/ebl/repository/CargoLineItemRepository.java
@@ -1,16 +1,15 @@
 package org.dcsa.ebl.repository;
 
-import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.ebl.model.CargoLineItem;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface CargoLineItemRepository extends ExtendedRepository<CargoLineItem, UUID> {
+public interface CargoLineItemRepository extends R2dbcRepository<CargoLineItem, UUID>, InsertAddonRepository<CargoLineItem> {
 
     Flux<CargoLineItem> findAllByCargoItemID(UUID cargoItemID);
     Mono<Void> deleteByCargoItemIDAndCargoLineItemIDIn(UUID cargoItemID, List<String> cargoLineItemIDs);
-    Flux<CargoLineItem> findAllByCargoItemIDAndCargoLineItemIDInOrderByCargoLineItemID(UUID cargoItemID, List<String> cargoLineItemIDs);
 }
