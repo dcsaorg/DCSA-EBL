5ba61f289827dde4440c2c3464bd0b1c862d8453
Niels Thykier <nt@asseco.dk>
2021-02-12 15:42:20 +0100

    SI: Map freightPayableAt on GET/POST/PUT
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/repository/LocationRepository.java
+++ src/main/java/org/dcsa/ebl/repository/LocationRepository.java
@@ -1,11 +1,17 @@
 package org.dcsa.ebl.repository;
 
 import org.dcsa.core.repository.ExtendedRepository;
-import org.dcsa.ebl.model.CargoItem;
 import org.dcsa.ebl.model.Location;
+import org.springframework.data.r2dbc.repository.Query;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface LocationRepository extends ExtendedRepository<Location, UUID> {
-
+    @Query("SELECT location.*"
+            + "  FROM location"
+            + "  JOIN shipping_instruction ON (location.id=shipping_instruction.freight_payable_at)"
+            + " WHERE shipping_instruction.id = :shippingInstructionID"
+    )
+    Mono<Location> findPaymentLocationByShippingInstructionID(UUID shippingInstructionID);
 }
--- src/main/java/org/dcsa/ebl/service/impl/LocationServiceImpl.java
+++ src/main/java/org/dcsa/ebl/service/impl/LocationServiceImpl.java
@@ -1,12 +1,14 @@
 package org.dcsa.ebl.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.dcsa.core.exception.UpdateException;
 import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
 import org.dcsa.ebl.model.Location;
 import org.dcsa.ebl.repository.LocationRepository;
 import org.dcsa.ebl.service.LocationService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -25,8 +27,25 @@ public class LocationServiceImpl extends ExtendedBaseServiceImpl<LocationReposit
         return Location.class;
     }
 
+    public Mono<Location> findPaymentLocationByShippingInstructionID(UUID shippingInstructionID) {
+        return locationRepository.findPaymentLocationByShippingInstructionID(shippingInstructionID);
+    }
 
-    public Flux<Location> findAllById(Iterable<UUID> ids) {
-        return locationRepository.findAllById(ids);
+    public Mono<Location> resolveLocation(Location location) {
+        Mono<Location> locationMono;
+        if (location.getId() != null) {
+            locationMono = findById(location.getId())
+                    .flatMap(resolvedLocation -> {
+                        if (!location.containsOnlyID() && !resolvedLocation.equals(location)) {
+                            return Mono.error(new UpdateException("Location with id " + location.getId()
+                                    + " exists but has different content. Remove the locationID field to"
+                                    + " create a new instance or provide an update"));
+                        }
+                        return Mono.just(resolvedLocation);
+                    });
+        } else {
+            locationMono = create(location);
+        }
+        return locationMono.doOnNext(resolvedLocation -> location.setId(resolvedLocation.getId()));
     }
 }
