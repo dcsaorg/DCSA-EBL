eaa742f9350d66e12a9ff09c35334e1b40671baa
Niels Thykier <nt@asseco.dk>
2021-02-22 15:34:11 +0100

    Remove isPartLoad from SI
    To match DCSA-Information-Model#38
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/model/base/AbstractShippingInstruction.java
+++ src/main/java/org/dcsa/ebl/model/base/AbstractShippingInstruction.java
@@ -31,9 +31,6 @@ public abstract class AbstractShippingInstruction extends AuditBase implements G
     @Column("is_electronic")
     private Boolean isElectronic;
 
-    @Column("is_part_load")
-    private Boolean isPartLoad;
-
     @Column("is_shipped_onboard_type")
     @NotNull
     private Boolean isShippedOnBoardType;
