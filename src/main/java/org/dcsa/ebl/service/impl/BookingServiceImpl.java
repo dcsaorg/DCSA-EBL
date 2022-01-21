feab2cdef3ad4b1f60b451ce98cff4ef1a202701
Martin Petersson <mp@asseco.dk>
2022-01-21 12:58:18 +0100

    Update BookingServiceImpl.java

--- src/main/java/org/dcsa/ebl/service/impl/BookingServiceImpl.java
+++ src/main/java/org/dcsa/ebl/service/impl/BookingServiceImpl.java
@@ -1,21 +1,22 @@
-package org.dcsa.ebl.service.impl;
-
-import lombok.RequiredArgsConstructor;
-import org.dcsa.core.events.model.Booking;
-import org.dcsa.core.events.repository.BookingRepository;
-import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
-import org.dcsa.ebl.service.BookingService;
-import org.springframework.stereotype.Service;
-
-import java.util.UUID;
-
-@RequiredArgsConstructor
-@Service
-public class BookingServiceImpl extends ExtendedBaseServiceImpl<BookingRepository, Booking, UUID> implements BookingService {
-    private final BookingRepository bookingRepository;
-
-    @Override
-    public BookingRepository getRepository() {
-        return bookingRepository;
-    }
-}
+//package org.dcsa.ebl.service.impl;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.dcsa.core.events.model.Booking;
+//import org.dcsa.core.events.repository.BookingRepository;
+//import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
+//import org.dcsa.ebl.service.BookingService;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.UUID;
+//
+//@RequiredArgsConstructor
+//@Service
+// TODO: fix me
+//public class BookingServiceImpl extends ExtendedBaseServiceImpl<BookingRepository, Booking, UUID> implements BookingService {
+//    private final BookingRepository bookingRepository;
+//
+//    @Override
+//    public BookingRepository getRepository() {
+//        return bookingRepository;
+//    }
+//}
