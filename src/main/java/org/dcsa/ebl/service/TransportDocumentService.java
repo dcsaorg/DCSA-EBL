8c439969503d486821158dc2593eb715e243cae5
pimg <pim.gaemers@dcsa.org>
2022-02-21 09:09:43 +0100

    DDT-860 cleaned old implementation get transport document by id

--- src/main/java/org/dcsa/ebl/service/TransportDocumentService.java
+++ src/main/java/org/dcsa/ebl/service/TransportDocumentService.java
@@ -1,8 +1,12 @@
 package org.dcsa.ebl.service;
 
+
 import org.dcsa.core.events.model.TransportDocument;
 import org.dcsa.core.service.QueryService;
+import org.dcsa.ebl.model.transferobjects.TransportDocumentTO;
+import reactor.core.publisher.Mono;
 
 public interface TransportDocumentService extends QueryService<TransportDocument, String> {
 
+	Mono<TransportDocumentTO> findById(String transportDocumentReference);
 }
