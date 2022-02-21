2bbd21da2b1f9e5f18c8d5ccb19bba62e88befad
pimg <pim.gaemers@dcsa.org>
2022-02-21 09:09:43 +0100

    DDT-860 cleaned old implementation get transport document by id

--- src/main/java/org/dcsa/ebl/controller/TransportDocumentController.java
+++ src/main/java/org/dcsa/ebl/controller/TransportDocumentController.java
@@ -1,54 +1,24 @@
 package org.dcsa.ebl.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.dcsa.core.events.model.TransportDocument;
-import org.dcsa.core.exception.GetException;
-import org.dcsa.core.extendedrequest.ExtendedParameters;
-import org.dcsa.core.extendedrequest.ExtendedRequest;
-import org.dcsa.ebl.extendedrequest.TransportDocumentExtendedRequest;
 import org.dcsa.ebl.model.transferobjects.TransportDocumentTO;
-import org.dcsa.ebl.service.TransportDocumentTOService;
-import org.springframework.data.r2dbc.dialect.R2dbcDialect;
-import org.springframework.http.HttpStatus;
+import org.dcsa.ebl.service.TransportDocumentService;
 import org.springframework.http.MediaType;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "transport-documents", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class TransportDocumentController {
 
-    private final ExtendedParameters extendedParameters;
-
-    private final TransportDocumentTOService transportDocumentTOService;
-
-    private final R2dbcDialect r2dbcDialect;
-
-    @GetMapping
-    public Flux<TransportDocument> findAll(ServerHttpResponse response, ServerHttpRequest request) {
-        ExtendedRequest<TransportDocument> extendedRequest = new TransportDocumentExtendedRequest<>(extendedParameters,
-                r2dbcDialect, TransportDocument.class);
-
-        try {
-            extendedRequest.parseParameter(request.getQueryParams());
-        } catch (GetException e) {
-            return Flux.error(e);
-        }
-
-        return transportDocumentTOService.findAllExtended(extendedRequest).doOnComplete(
-                () -> extendedRequest.insertHeaders(response, request)
-        );
-    }
+    private final TransportDocumentService transportDocumentService;
 
     @GetMapping(path="{transportDocumentReference}")
     public Mono<TransportDocumentTO> findById(@PathVariable String transportDocumentReference) {
-        return transportDocumentTOService.findById(transportDocumentReference);
+        return transportDocumentService.findById(transportDocumentReference);
     }
 }
--- src/main/java/org/dcsa/ebl/service/impl/TransportDocumentServiceImpl.java
+++ src/main/java/org/dcsa/ebl/service/impl/TransportDocumentServiceImpl.java
@@ -4,17 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.dcsa.core.events.model.TransportDocument;
 import org.dcsa.core.events.repository.TransportDocumentRepository;
 import org.dcsa.core.service.impl.QueryServiceImpl;
+import org.dcsa.ebl.model.transferobjects.TransportDocumentTO;
 import org.dcsa.ebl.service.TransportDocumentService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Service
 public class TransportDocumentServiceImpl extends QueryServiceImpl<TransportDocumentRepository, TransportDocument, String> implements TransportDocumentService {
 
+
     private final TransportDocumentRepository transportDocumentRepository;
 
+  @Override
+  public TransportDocumentRepository getRepository() {
+    return transportDocumentRepository;
+  }
+
     @Override
-    public TransportDocumentRepository getRepository() {
-        return transportDocumentRepository;
+    public Mono<TransportDocumentTO> findById(String transportDocumentReference) {
+        return null;
     }
 }
