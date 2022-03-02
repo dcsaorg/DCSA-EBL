f4f25428986ca68b10cbbee3f2823505d13eb666
Martin Petersson <mp@asseco.dk>
2022-03-02 11:38:34 +0100

    Removed unused functions

--- src/main/java/org/dcsa/ebl/service/impl/TransportDocumentServiceImpl.java
+++ src/main/java/org/dcsa/ebl/service/impl/TransportDocumentServiceImpl.java
@@ -1,20 +1,15 @@
 package org.dcsa.ebl.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.dcsa.core.events.edocumentation.model.transferobject.ChargeTO;
 import org.dcsa.core.events.edocumentation.service.CarrierClauseService;
 import org.dcsa.core.events.edocumentation.service.ChargeService;
-import org.dcsa.core.events.model.Booking;
 import org.dcsa.core.events.model.Carrier;
-import org.dcsa.core.events.model.Charge;
 import org.dcsa.core.events.model.TransportDocument;
 import org.dcsa.core.events.model.enums.CarrierCodeListProvider;
-import org.dcsa.core.events.repository.BookingRepository;
 import org.dcsa.core.events.repository.CarrierRepository;
 import org.dcsa.core.events.repository.TransportDocumentRepository;
 import org.dcsa.core.events.service.LocationService;
 import org.dcsa.core.exception.ConcreteRequestErrorMessageException;
-import org.dcsa.core.exception.CreateException;
 import org.dcsa.core.service.impl.AsymmetricQueryServiceImpl;
 import org.dcsa.ebl.model.TransportDocumentSummary;
 import org.dcsa.ebl.model.mappers.TransportDocumentMapper;
@@ -24,11 +19,8 @@ import org.dcsa.ebl.service.ShippingInstructionService;
 import org.dcsa.ebl.service.TransportDocumentService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 @RequiredArgsConstructor
@@ -39,7 +31,6 @@ public class TransportDocumentServiceImpl
     implements TransportDocumentService {
 
   private final TransportDocumentRepository transportDocumentRepository;
-  private final BookingRepository bookingRepository;
   private final CarrierRepository carrierRepository;
   private final ShippingInstructionRepository shippingInstructionRepository;
 
@@ -50,64 +41,14 @@ public class TransportDocumentServiceImpl
 
   private final TransportDocumentMapper transportDocumentMapper;
 
-  /**
-   * Checks if TransportDocument differs from Booking with regard to ServiceType, ShipmentTerms and
-   * ContractService. Sets the above-mentioned values from Booking on TransportDocument
-   *
-   * @param carrierBookingReference the reference to the Booking
-   * @param transportDocumentTO the TransportDocument to update
-   * @return Error if a discrepancy is found otherwise returns the Booking
-   */
-  private Mono<Booking> updateTransportDocumentWithBookingInfo(
-      String carrierBookingReference, TransportDocumentTO transportDocumentTO) {
-
-    return Mono.empty();
-  }
-
-  private <T> Mono<T> getBookingError(String fieldName, String fromValue, String toValue) {
-    return Mono.error(
-        new CreateException(
-            "It is not possible to change "
-                + fieldName
-                + " from "
-                + fromValue
-                + " to "
-                + toValue
-                + " when creating a new TransportDocument. Please change this via booking"));
-  }
-
-  private Flux<Charge> createCharges(
-      TransportDocumentTO transportDocumentTO, boolean isChargesDisplayed) {
-    List<ChargeTO> chargeTOs = transportDocumentTO.getCharges();
-    if (chargeTOs == null || chargeTOs.isEmpty()) {
-      // In case a new TransportDocument is created it is intentional to send an empty
-      // array if no charges are included...
-      transportDocumentTO.setCharges(Collections.emptyList());
-      return Flux.empty();
-    } else {
-      if (!isChargesDisplayed) {
-        return Flux.error(
-            new CreateException(
-                "isDisplayCharges is set to false on ShippingInstruction - it is not possible to create new Charges as they will not be part of the TransportDocument"));
-      } else {
-        return Flux.empty();
-      }
-    }
+  @Override
+  public TransportDocumentRepository getRepository() {
+    return transportDocumentRepository;
   }
 
   @Transactional
   @Override
   public Mono<TransportDocumentTO> findById(String transportDocumentReference) {
-    return Mono.empty();
-  }
-
-  private Mono<Void> updateTransportDocumentWithCharges(
-      TransportDocumentTO transportDocumentTO, boolean isChargesDisplayed) {
-    return Mono.empty();
-  }
-
-  private Mono<Void> updateTransportDocumentWithTransportPlan(
-      String carrierBookingReference, TransportDocumentTO transportDocumentTO) {
     return Mono.empty();
   }
 
@@ -160,26 +101,6 @@ public class TransportDocumentServiceImpl
                         return Mono.just(transportDocumentSummary);
                       });
             });
-  }
-
-  private Mono<Booking> getBooking(String carrierBookingReference, String shippingInstructionID) {
-    // Don't use ServiceClass - use Repository directly in order to throw internal error if
-    // BookingReference does not exist.
-    return bookingRepository
-        .findByCarrierBookingRequestReference(carrierBookingReference)
-        .switchIfEmpty(
-            Mono.error(
-                new IllegalStateException(
-                    "The CarrierBookingReference: "
-                        + carrierBookingReference
-                        + " specified on ShippingInstruction:"
-                        + shippingInstructionID
-                        + " does not exist!")));
-  }
-
-  @Override
-  public TransportDocumentRepository getRepository() {
-    return transportDocumentRepository;
   }
 
   @Override
