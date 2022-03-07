4ae831b0edd6ad391cab9837b4ff446bea48e0f8
Martin Petersson <mp@asseco.dk>
2022-03-07 14:09:05 +0100

    DDT-873: GET SI Integration Tests

--- src/test/java/ebl/v2/ShippingInstructionIT.java
+++ src/test/java/ebl/v2/ShippingInstructionIT.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ebl.config.TestConfig;
 import org.apache.http.HttpStatus;
 import org.dcsa.core.events.model.transferobjects.ShippingInstructionTO;
+import org.dcsa.ebl.model.transferobjects.ShippingInstructionResponseTO;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,8 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
-import static ebl.config.TestConfig.*;
+import static ebl.config.TestConfig.SHIPPING_INSTRUCTIONS;
+import static ebl.config.TestConfig.jsonSchemaValidator;
 import static ebl.config.TestUtil.jsonToMap;
 import static ebl.config.TestUtil.loadFileAsString;
 import static io.restassured.RestAssured.given;
@@ -24,10 +26,14 @@ class ShippingInstructionIT {
       loadFileAsString("ValidShippingInstruction.json");
 
   private final ObjectMapper objectMapper = new ObjectMapper();
+  private static ShippingInstructionTO shippingInstructionTO;
 
   @BeforeAll
   static void configs() throws IOException {
     TestConfig.init();
+
+    shippingInstructionTO = new ShippingInstructionTO();
+    shippingInstructionTO.setCarrierBookingReference(UUID.randomUUID().toString());
   }
 
   @Test
@@ -97,5 +103,62 @@ class ShippingInstructionIT {
         .extract()
         .body()
         .asString();
+  }
+
+  @Test
+  void testInvalidGetShippingInstruction() {
+
+    UUID invalidShippingInstructionReference = UUID.randomUUID();
+
+    given()
+        .contentType("application/json")
+        .get(SHIPPING_INSTRUCTIONS + "/" + invalidShippingInstructionReference)
+        .then()
+        .assertThat()
+        .statusCode(HttpStatus.SC_NOT_FOUND)
+        .body("httpMethod", equalTo("GET"))
+        .body("requestUri", containsString("/v2/shipping-instructions"))
+        .body("errors[0].reason", equalTo("notFound"))
+        .body(
+            "errors[0].message",
+            containsString(
+                "No Shipping Instruction found with ID: " + invalidShippingInstructionReference))
+        .body("statusCode", equalTo(404))
+        .body("statusCodeText", equalTo("Not Found"))
+        .extract()
+        .body()
+        .asString();
+  }
+
+  @Test
+  void testValidGetShippingInstruction() throws IOException {
+    ShippingInstructionResponseTO response = createShippingInstruction();
+
+    given()
+        .contentType("application/json")
+        .get(SHIPPING_INSTRUCTIONS + "/" + response.getShippingInstructionReference())
+        .then()
+        .assertThat()
+        .statusCode(HttpStatus.SC_OK)
+        .body("httpMethod", equalTo("GET"))
+        .body("requestUri", containsString("/v2/shipping-instructions"))
+        .body("shippingInstructionReference", notNullValue())
+        .body(jsonSchemaValidator("shippingInstruction"))
+        .extract()
+        .body()
+        .asString();
+  }
+
+  ShippingInstructionResponseTO createShippingInstruction() throws JsonProcessingException {
+    return given()
+        .contentType("application/json")
+        .body(loadFileAsString("ValidShippingInstructionHack.json"))
+        .post(SHIPPING_INSTRUCTIONS)
+        .then()
+        .assertThat()
+        .statusCode(HttpStatus.SC_CREATED)
+        .extract()
+        .response()
+        .as(ShippingInstructionResponseTO.class);
   }
 }
