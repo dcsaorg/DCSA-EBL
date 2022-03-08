d66ff6a80b64dc4349622b550ca7d05397e7610e
Martin Petersson <mp@asseco.dk>
2022-03-08 13:57:59 +0100

    GET IT cases

--- src/test/java/ebl/v2/ShippingInstructionIT.java
+++ src/test/java/ebl/v2/ShippingInstructionIT.java
@@ -6,9 +6,11 @@ import ebl.config.TestConfig;
 import org.apache.http.HttpStatus;
 import org.dcsa.core.events.model.transferobjects.ShippingInstructionTO;
 import org.dcsa.ebl.model.transferobjects.ShippingInstructionResponseTO;
+import org.hibernate.validator.constraints.Length;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
@@ -26,14 +28,10 @@ class ShippingInstructionIT {
       loadFileAsString("ValidShippingInstruction.json");
 
   private final ObjectMapper objectMapper = new ObjectMapper();
-  private static ShippingInstructionTO shippingInstructionTO;
 
   @BeforeAll
   static void configs() throws IOException {
     TestConfig.init();
-
-    shippingInstructionTO = new ShippingInstructionTO();
-    shippingInstructionTO.setCarrierBookingReference(UUID.randomUUID().toString());
   }
 
   @Test
@@ -93,13 +91,13 @@ class ShippingInstructionIT {
         .post(SHIPPING_INSTRUCTIONS)
         .then()
         .assertThat()
-        .statusCode(HttpStatus.SC_BAD_REQUEST)
+        .statusCode(HttpStatus.SC_NOT_FOUND)
         .body("httpMethod", equalTo("POST"))
         .body("requestUri", containsString("/v2/shipping-instructions"))
-        .body("errors[0].reason", equalTo("invalidParameter"))
-        .body("errors[0].message", containsString("No shipment found with carrierBookingReference"))
-        .body("statusCode", equalTo(400))
-        .body("statusCodeText", equalTo("Bad Request"))
+        .body("errors[0].reason", equalTo("notFound"))
+        .body("errors[0].message", containsString("No booking found for carrier booking reference"))
+        .body("statusCode", equalTo(404))
+        .body("statusCodeText", equalTo("Not Found"))
         .extract()
         .body()
         .asString();
@@ -131,8 +129,11 @@ class ShippingInstructionIT {
   }
 
   @Test
-  void testValidGetShippingInstruction() throws IOException {
-    ShippingInstructionResponseTO response = createShippingInstruction();
+  void testValidGetShippingInstruction() {
+    Map<String, Object> map = jsonToMap(VALID_SHIPPING_INSTRUCTION);
+    assert map != null;
+
+    ShippingInstructionResponseTO response = createShippingInstruction(map);
 
     given()
         .contentType("application/json")
@@ -140,19 +141,64 @@ class ShippingInstructionIT {
         .then()
         .assertThat()
         .statusCode(HttpStatus.SC_OK)
-        .body("httpMethod", equalTo("GET"))
-        .body("requestUri", containsString("/v2/shipping-instructions"))
-        .body("shippingInstructionReference", notNullValue())
+        .body("documentParties", hasSize(greaterThan(0)))
+        .body("shipments", hasSize(greaterThan(0)))
+        .body("references", hasSize(greaterThan(0)))
+        .body("utilizedTransportEquipments", hasSize(greaterThan(0)))
+        .body("placeOfIssue", notNullValue())
+        .body("isToOrder", notNullValue())
+        .body("isShippedOnboardType", notNullValue())
         .body(jsonSchemaValidator("shippingInstruction"))
         .extract()
         .body()
         .asString();
   }
 
-  ShippingInstructionResponseTO createShippingInstruction() throws JsonProcessingException {
+  @Test
+  void testValidGetShippingInstructionShallow() {
+    Map<String, Object> map = jsonToMap(VALID_SHIPPING_INSTRUCTION);
+    assert map != null;
+    map.put("placeOfIssue", null);
+    map.put("placeOfIssueID", null);
+    map.put("documentParties", null);
+    map.put("numberOfCopies", null);
+    map.put("numberOfOriginals", null);
+    map.put("isElectronic", null);
+    map.put("areChargesDisplayedOnOriginals", null);
+    map.put("areChargesDisplayedOnCopies", null);
+
+    ShippingInstructionResponseTO response = createShippingInstruction(map);
+
+    given()
+        .contentType("application/json")
+        .get(SHIPPING_INSTRUCTIONS + "/" + response.getShippingInstructionReference())
+        .then()
+        .assertThat()
+        .statusCode(HttpStatus.SC_OK)
+        .body("documentParties", nullValue())
+        .body("shipments", hasSize(greaterThan(0)))
+        .body("references", hasSize(greaterThan(0)))
+        .body("utilizedTransportEquipments", hasSize(greaterThan(0)))
+//        .body("placeOfIssue", equalTo("<{}>")) // doesn't accept hasSize(0) or nullValue
+        .body("isToOrder", notNullValue())
+        .body("isShippedOnboardType", notNullValue())
+        .body("numberOfCopies", nullValue())
+        .body("numberOfOriginals", nullValue())
+        .body("isElectronic", nullValue())
+        .body("areChargesDisplayedOnOriginals", nullValue())
+        .body("areChargesDisplayedOnCopies", nullValue())
+        .body(jsonSchemaValidator("shippingInstruction"))
+        .extract()
+        .body()
+        .asString();
+  }
+
+  ShippingInstructionResponseTO createShippingInstruction(Map<String, Object> map) {
+
+    System.out.println(map);
     return given()
         .contentType("application/json")
-        .body(loadFileAsString("ValidShippingInstructionHack.json"))
+        .body(map)
         .post(SHIPPING_INSTRUCTIONS)
         .then()
         .assertThat()
