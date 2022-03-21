4e352ea51f68fa6f0e9b1b6a82f46e1ba5100a8b
Martin Petersson <mp@asseco.dk>
2022-03-21 11:37:50 +0100

    Hopefully get at log on validation fail in eventIT

--- src/test/java/ebl/v2/ShipmentEventIT.java
+++ src/test/java/ebl/v2/ShipmentEventIT.java
@@ -1,6 +1,7 @@
 package ebl.v2;
 
 import ebl.config.TestConfig;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import lombok.RequiredArgsConstructor;
 import org.hamcrest.BaseMatcher;
@@ -31,34 +32,37 @@ class ShipmentEventIT {
   @Test
   void testGetAllEvents() {
     given()
-      .contentType("application/json")
-      .get("/v2/events")
-      .then()
-      .assertThat()
-      .statusCode(200)
-      .contentType(ContentType.JSON)
-      .body("size()", greaterThanOrEqualTo(0))
-      .body("eventType", everyItem(equalTo("SHIPMENT")))
-      .body("eventClassifierCode", everyItem(equalTo("ACT")))
-      ;
-  }
-
-  @Test
-  void testGetAllEventsByShipmentEventTypeCode() {
-    BiConsumer<String, Matcher<String>> runner = (s, m) ->
-        given()
         .contentType("application/json")
-        .queryParam("shipmentEventTypeCode", s)
         .get("/v2/events")
         .then()
+        .log()
+        .ifValidationFails(LogDetail.BODY)
         .assertThat()
         .statusCode(200)
         .contentType(ContentType.JSON)
         .body("size()", greaterThanOrEqualTo(0))
         .body("eventType", everyItem(equalTo("SHIPMENT")))
-        .body("eventClassifierCode", everyItem(equalTo("ACT")))
-        .body("shipmentEventTypeCode", everyItem(m))
-    ;
+        .body("eventClassifierCode", everyItem(equalTo("ACT")));
+  }
+
+  @Test
+  void testGetAllEventsByShipmentEventTypeCode() {
+    BiConsumer<String, Matcher<String>> runner =
+        (s, m) ->
+            given()
+                .contentType("application/json")
+                .queryParam("shipmentEventTypeCode", s)
+                .get("/v2/events")
+                .then()
+                .log()
+                .ifValidationFails(LogDetail.BODY)
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", greaterThanOrEqualTo(0))
+                .body("eventType", everyItem(equalTo("SHIPMENT")))
+                .body("eventClassifierCode", everyItem(equalTo("ACT")))
+                .body("shipmentEventTypeCode", everyItem(m));
 
     runner.accept("APPR,ISSU", anyOf(equalTo("APPR"), equalTo("ISSU")));
     runner.accept("APPR", equalTo("APPR"));
@@ -67,20 +71,22 @@ class ShipmentEventIT {
 
   @Test
   void testGetAllEventsByDocumentTypeCodeCode() {
-    BiConsumer<String, Matcher<String>> runner = (s, m) ->
-      given()
-        .contentType("application/json")
-        .queryParam("documentTypeCode", s)
-        .get("/v2/events")
-        .then()
-        .assertThat()
-        .statusCode(200)
-        .contentType(ContentType.JSON)
-        .body("size()", greaterThanOrEqualTo(0))
-        .body("eventType", everyItem(equalTo("SHIPMENT")))
-        .body("eventClassifierCode", everyItem(equalTo("ACT")))
-        .body("documentTypeCode", everyItem(m))
-      ;
+    BiConsumer<String, Matcher<String>> runner =
+        (s, m) ->
+            given()
+                .contentType("application/json")
+                .queryParam("documentTypeCode", s)
+                .get("/v2/events")
+                .then()
+                .log()
+                .ifValidationFails(LogDetail.BODY)
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", greaterThanOrEqualTo(0))
+                .body("eventType", everyItem(equalTo("SHIPMENT")))
+                .body("eventClassifierCode", everyItem(equalTo("ACT")))
+                .body("documentTypeCode", everyItem(m));
     runner.accept("SHI,TRD", anyOf(equalTo("SHI"), equalTo("TRD")));
     runner.accept("SHI", equalTo("SHI"));
     runner.accept("TRD", equalTo("TRD"));
@@ -89,36 +95,41 @@ class ShipmentEventIT {
   @Test
   void testGetAllEventsByCombinedQuery() {
     given()
-      .contentType("application/json")
-      // VOID only applies to TRD, so this is guaranteed to give 0 matches.
-      .queryParam("documentTypeCode", "SHI")
-      .queryParam("shipmentEventTypeCode", "VOID")
-      .get("/v2/events")
-      .then()
-      .assertThat()
-      .statusCode(200)
-      .contentType(ContentType.JSON)
-      .body("size()", equalTo(0))
-    ;
+        .contentType("application/json")
+        // VOID only applies to TRD, so this is guaranteed to give 0 matches.
+        .queryParam("documentTypeCode", "SHI")
+        .queryParam("shipmentEventTypeCode", "VOID")
+        .get("/v2/events")
+        .then()
+        .log()
+        .ifValidationFails(LogDetail.BODY)
+        .assertThat()
+        .statusCode(200)
+        .contentType(ContentType.JSON)
+        .body("size()", equalTo(0));
   }
 
   @Test
   void testGetAllEventsByCarrierBookingReference() {
     given()
-      .contentType("application/json")
-      .queryParam("carrierBookingReference", "832deb4bd4ea4b728430b857c59bd057")
-      .get("/v2/events")
-      .then()
-      .assertThat()
-      .statusCode(200)
-      .contentType(ContentType.JSON)
-      // The test data includes at least 3 shipment events related to the reference. But something adding additional
-      // events.
-      .body("size()", greaterThanOrEqualTo(3))
-      .body("eventType", everyItem(equalTo("SHIPMENT")))
-      .body("eventClassifierCode", everyItem(equalTo("ACT")))
-      .body("documentTypeCode", everyItem(anyOf(equalTo("SHI"), equalTo("TRD"))))
-    // TODO: Assert that document references contain the carrier booking reference 832deb4bd4ea4b728430b857c59bd057
+        .contentType("application/json")
+        .queryParam("carrierBookingReference", "832deb4bd4ea4b728430b857c59bd057")
+        .get("/v2/events")
+        .then()
+        .log()
+        .ifValidationFails(LogDetail.BODY)
+        .assertThat()
+        .statusCode(200)
+        .contentType(ContentType.JSON)
+        // The test data includes at least 3 shipment events related to the reference. But something
+        // adding additional
+        // events.
+        .body("size()", greaterThanOrEqualTo(3))
+        .body("eventType", everyItem(equalTo("SHIPMENT")))
+        .body("eventClassifierCode", everyItem(equalTo("ACT")))
+        .body("documentTypeCode", everyItem(anyOf(equalTo("SHI"), equalTo("TRD"))))
+    // TODO: Assert that document references contain the carrier booking reference
+    // 832deb4bd4ea4b728430b857c59bd057
     // (Needs a fix for DDT-928 first)
     ;
   }
@@ -128,75 +139,95 @@ class ShipmentEventIT {
     String rangeStart = "2021-01-08T00:00:00Z";
     String rangeEnd = "2021-01-09T00:00:00Z";
     given()
-      .contentType("application/json")
-      .queryParam("carrierBookingReference", "832deb4bd4ea4b728430b857c59bd057")
-      .queryParam("eventCreatedDateTime:gte", rangeStart)
-      .queryParam("eventCreatedDateTime:lt", rangeEnd)
-      .get("/v2/events")
-      .then()
-      .assertThat()
-      .statusCode(200)
-      .contentType(ContentType.JSON)
-      // The test data includes 3 shipment events for this case. Given the narrow date range, it seems acceptable to
-      // validate an exact match.
-      .body("size()", equalTo(3))
-      .body("eventType", everyItem(equalTo("SHIPMENT")))
-      .body("eventClassifierCode", everyItem(equalTo("ACT")))
-      .body("documentTypeCode", everyItem(anyOf(equalTo("SHI"), equalTo("TRD"))))
-      .body("eventCreatedDateTime", everyItem(asDateTime(allOf(
-            greaterThanOrEqualTo(ZonedDateTime.parse(rangeStart)),
-            lessThan(ZonedDateTime.parse(rangeEnd))
-      ))))
-      // TODO: Assert that document references contain the carrier booking reference 832deb4bd4ea4b728430b857c59bd057
-      // (Needs a fix for DDT-928 first)
+        .contentType("application/json")
+        .queryParam("carrierBookingReference", "832deb4bd4ea4b728430b857c59bd057")
+        .queryParam("eventCreatedDateTime:gte", rangeStart)
+        .queryParam("eventCreatedDateTime:lt", rangeEnd)
+        .get("/v2/events")
+        .then()
+        .log()
+        .ifValidationFails(LogDetail.BODY)
+        .assertThat()
+        .statusCode(200)
+        .contentType(ContentType.JSON)
+        // The test data includes 3 shipment events for this case. Given the narrow date range, it
+        // seems acceptable to
+        // validate an exact match.
+        .body("size()", equalTo(3))
+        .body("eventType", everyItem(equalTo("SHIPMENT")))
+        .body("eventClassifierCode", everyItem(equalTo("ACT")))
+        .body("documentTypeCode", everyItem(anyOf(equalTo("SHI"), equalTo("TRD"))))
+        .body(
+            "eventCreatedDateTime",
+            everyItem(
+                asDateTime(
+                    allOf(
+                        greaterThanOrEqualTo(ZonedDateTime.parse(rangeStart)),
+                        lessThan(ZonedDateTime.parse(rangeEnd))))))
+    // TODO: Assert that document references contain the carrier booking reference
+    // 832deb4bd4ea4b728430b857c59bd057
+    // (Needs a fix for DDT-928 first)
     ;
   }
 
   @Test
   void testGetAllEventsByEventCreatedDateTimeRange() {
     String rangeStart = "2021-01-08T00:00:00Z";
-    // 10:00-0400 is 14:00 at Z, so the first event for CBR 832deb4bd4ea4b728430b857c59bd057 is included while the
+    // 10:00-0400 is 14:00 at Z, so the first event for CBR 832deb4bd4ea4b728430b857c59bd057 is
+    // included while the
     // latter to are excluded
     String rangeEnd = "2021-01-08T10:00:00-04:00";
     given()
-      .contentType("application/json")
-      .queryParam("eventCreatedDateTime:gte", rangeStart)
-      .queryParam("eventCreatedDateTime:lt", rangeEnd)
-      .get("/v2/events")
-      .then()
-      .assertThat()
-      .statusCode(200)
-      .contentType(ContentType.JSON)
-      // The test data includes 1 shipment event for this case. Given the narrow date range, it seems acceptable to
-      // validate an exact match.  Note the strict match is used to validate that the TZ conversion works correctly
-      // when filtering
-      .body("size()", equalTo(1))
-      .body("eventType", everyItem(equalTo("SHIPMENT")))
-      .body("eventClassifierCode", everyItem(equalTo("ACT")))
-      .body("documentTypeCode", everyItem(anyOf(equalTo("SHI"), equalTo("TRD"))))
-      .body("eventCreatedDateTime", everyItem(asDateTime(allOf(
-        greaterThanOrEqualTo(ZonedDateTime.parse(rangeStart)),
-        lessThan(ZonedDateTime.parse(rangeEnd))
-      ))))
-    // TODO: Assert that document references contain the carrier booking reference 832deb4bd4ea4b728430b857c59bd057
+        .contentType("application/json")
+        .queryParam("eventCreatedDateTime:gte", rangeStart)
+        .queryParam("eventCreatedDateTime:lt", rangeEnd)
+        .get("/v2/events")
+        .then()
+        .log()
+        .ifValidationFails(LogDetail.BODY)
+        .assertThat()
+        .statusCode(200)
+        .contentType(ContentType.JSON)
+        // The test data includes 1 shipment event for this case. Given the narrow date range, it
+        // seems acceptable to
+        // validate an exact match.  Note the strict match is used to validate that the TZ
+        // conversion works correctly
+        // when filtering
+        .body("size()", equalTo(1))
+        .body("eventType", everyItem(equalTo("SHIPMENT")))
+        .body("eventClassifierCode", everyItem(equalTo("ACT")))
+        .body("documentTypeCode", everyItem(anyOf(equalTo("SHI"), equalTo("TRD"))))
+        .body(
+            "eventCreatedDateTime",
+            everyItem(
+                asDateTime(
+                    allOf(
+                        greaterThanOrEqualTo(ZonedDateTime.parse(rangeStart)),
+                        lessThan(ZonedDateTime.parse(rangeEnd))))))
+    // TODO: Assert that document references contain the carrier booking reference
+    // 832deb4bd4ea4b728430b857c59bd057
     // (Needs a fix for DDT-928 first)
-    // Note: The carrier booking reference must be present in the range due to the date range (but others could
+    // Note: The carrier booking reference must be present in the range due to the date range (but
+    // others could
     // be present as well).
     ;
   }
 
   /**
-   * Convert the input (assumed to be String) into a ZonedDateTime before chaining off to the next match
+   * Convert the input (assumed to be String) into a ZonedDateTime before chaining off to the next
+   * match
    *
-   * The conversion will use {@link ZonedDateTime#parse(CharSequence)}. If the parsing fails, the value
-   * is assumed not to match.
+   * <p>The conversion will use {@link ZonedDateTime#parse(CharSequence)}. If the parsing fails, the
+   * value is assumed not to match.
    *
    * @param dateTimeMatcher The matcher that should operator on a ZonedDateTime
    * @return The combined matcher
    */
-  // Use ChronoZonedDateTime as bound to avoid fighting generics with lessThan that "reduces" ZonedDateTime
+  // Use ChronoZonedDateTime as bound to avoid fighting generics with lessThan that "reduces"
+  // ZonedDateTime
   // to the ChronoZonedDateTime (via Comparable)
-  private static <T extends ChronoZonedDateTime<?>> Matcher<T> asDateTime(Matcher<T> dateTimeMatcher) {
+  private static <T extends ChronoZonedDateTime<?>> Matcher<T> asDateTime(
+      Matcher<T> dateTimeMatcher) {
     return new DateTimeMatcher<>(dateTimeMatcher);
   }
 
@@ -212,8 +243,8 @@ class ShipmentEventIT {
         return false;
       }
       try {
-        dateTime = ZonedDateTime.parse((String)actual);
-      } catch (DateTimeParseException e)  {
+        dateTime = ZonedDateTime.parse((String) actual);
+      } catch (DateTimeParseException e) {
         return false;
       }
       return matcher.matches(dateTime);
