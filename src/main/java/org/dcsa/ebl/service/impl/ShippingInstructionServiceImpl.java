496b9355a0580f2c2a7ad1979a87c779ed571000
Niels Thykier <nt@asseco.dk>
2022-04-29 09:29:53 +0200

    Remove "static" as requested in review comment
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ebl/service/impl/ShippingInstructionServiceImpl.java
+++ src/main/java/org/dcsa/ebl/service/impl/ShippingInstructionServiceImpl.java
@@ -504,7 +504,7 @@ public class ShippingInstructionServiceImpl implements ShippingInstructionServic
         shippingInstruction.getShippingInstructionUpdatedDateTime());
   }
 
-  private static Mono<ShipmentEvent> getShipmentEventFromShippingInstruction(
+  private Mono<ShipmentEvent> getShipmentEventFromShippingInstruction(
       String reason,
       ShipmentEventTypeCode documentStatus,
       UUID documentID,
